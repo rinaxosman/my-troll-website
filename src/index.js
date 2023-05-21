289fa6578e5f8353ef5014599245cd11c20d7f3b
rinaxosman <94803092+rinaxosman@users.noreply.github.com>
2023-05-21 03:32:59 -0400

    Update index.js to render TrollWebsite component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-
+import TrollWebsite from './TrollWebsite'; // Import the TrollWebsite component
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TrollWebsite /> {/* Render the TrollWebsite component */}
   </React.StrictMode>,
   document.getElementById('root')
 );
-
